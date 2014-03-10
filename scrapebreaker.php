85183110cac43d85fb77f660b31945c9124f8659
RedSand <RedSand@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-03-10 04:01:03 +0000

    Initial upload.
    git-svn-id: https://plugins.svn.wordpress.org/scrapebreaker/tags/1.0.0.0@872479 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ scrapebreaker.php
@@ -0,0 +1,62 @@
+<?php
+/*
+Plugin Name: ScrapeBreaker
+Plugin URI: http://www.redsandmarketing.com/plugins/scrapebreaker/
+Description: A combination of frame-breaker and scraper protection. Protect your website content from both frames and server-side scraping techniques. If either happens, visitors will be redirected to the original content.
+Author: Scott Allen
+Version: 1.0.0.0
+Author URI: http://www.redsandmarketing.com/
+License: GPLv2
+*/
+
+/*  Copyright 2014    Scott Allen  (email : plugins [at] redsandmarketing [dot] com)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+// PLUGIN - BEGIN
+
+/* Note to any other PHP developers reading this:
+My use of the end curly braces "}" is a little funky in that I indent them, I know. IMO it's easier to debug. Just know that it's on purpose even though it's not standard. One of my programming quirks, and just how I roll. :)
+*/
+
+// Setting constants in case we expand later on
+define( 'RSSB_VERSION', '1.0.0.0' );
+define( 'RSSB_REQUIRED_WP_VERSION', '2.5' );
+
+add_action( 'wp_head', 'rs_scrapebreaker', -10 );
+
+function rs_scrapebreaker() {
+	if (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] != 'off') {
+		$rs_this_page_prefix = 'https://';
+		}
+	else {
+		$rs_this_page_prefix = 'http://';
+		}
+	$rs_this_page_url = $rs_this_page_prefix.$_SERVER['SERVER_NAME'].$_SERVER['REQUEST_URI'];
+	if (is_admin()||current_user_can('level_10')){
+		return;	
+		}
+	echo '
+	<script type="text/javascript" async >
+	// <![CDATA[
+	var thispage = "'.$rs_this_page_url.'";
+	if (top.location!=thispage){top.location.href=thispage}
+	// ]]>
+	</script>
+';
+	}
+
+// PLUGIN - END
